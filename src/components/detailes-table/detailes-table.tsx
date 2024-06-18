e465df99a3af62a529fa99d7bc1820f89de323c3
“Olga <strelod@yandex.ru>
2024-06-18 19:12:56 +0300

    refactor: add components

---
+++ src/components/detailes-table/detailes-table.tsx
@@ -0,0 +1,127 @@
+import { useState, useEffect, FC } from "react";
+import { Button, Table } from "react-bootstrap";
+import styles from "./detailes-table.module.css";
+
+import { useAppDispatch, useAppSelector } from "../../hooks";
+import {
+  selectSelectedTrain,
+  updateTrainCharacteristic,
+} from "../../services/reducers/trainSlice";
+import { TrainCharacteristic } from "../../services/types";
+
+const DetailsTable: FC = () => {
+  const dispatch = useAppDispatch();
+
+  const selectedTrain = useAppSelector(selectSelectedTrain);
+  const [isFormValid, setIsFormValid] = useState(true);
+
+  useEffect(() => {
+    if (selectedTrain) {
+      setIsFormValid(
+        selectedTrain.characteristics.every(
+          (characteristic) =>
+            characteristic.speed >= 0 &&
+            characteristic.force > 0 &&
+            characteristic.engineAmperage > 0
+        )
+      );
+    }
+  }, [selectedTrain]);
+  const handleChange = (
+    index: number,
+    field: keyof TrainCharacteristic,
+    value: string
+  ) => {
+    let newCharacteristics = JSON.parse(
+      JSON.stringify(selectedTrain!.characteristics)
+    );
+    newCharacteristics[index][field] =
+      field === "speed" || field === "engineAmperage"
+        ? parseInt(value, 10)
+        : parseFloat(value);
+
+    dispatch(
+      updateTrainCharacteristic({
+        id: selectedTrain?.id!,
+        characteristics: newCharacteristics,
+      })
+    );
+  };
+
+  const handleSubmit = () => {
+    const sortedSpeedLimits = selectedTrain!.characteristics
+      .map((characteristic) => characteristic.speed)
+      .sort((a, b) => a - b);
+    console.log("Отсортированные ограничения скорости:", sortedSpeedLimits);
+  };
+
+  return (
+    <>
+      {selectedTrain && (
+        <section>
+          <h2>
+            Характеристики <br /> {selectedTrain.name}
+          </h2>
+          <Table bordered>
+            <thead>
+              <tr>
+                <th>Ток двигателя (А)</th>
+                <th>Сила тяги (кН)</th>
+                <th>Скорость (км/ч)</th>
+              </tr>
+            </thead>
+            <tbody>
+              {selectedTrain?.characteristics &&
+                selectedTrain.characteristics.map((characteristic, index) => (
+                  <tr key={index}>
+                    <td>
+                      <input
+                        required
+                        type="number"
+                        value={characteristic.speed || ""}
+                        onChange={(e) =>
+                          handleChange(index, "speed", e.target.value)
+                        }
+                        min={1}
+                        className={styles.input}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        required
+                        type="number"
+                        value={characteristic.force || ""}
+                        onChange={(e) =>
+                          handleChange(index, "force", e.target.value)
+                        }
+                        min="0.01"
+                        step="0.01"
+                        className={styles.input}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        required
+                        type="number"
+                        value={characteristic.engineAmperage || ""}
+                        onChange={(e) =>
+                          handleChange(index, "engineAmperage", e.target.value)
+                        }
+                        min={1}
+                        className={styles.input}
+                      />
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </Table>
+          <Button disabled={!isFormValid} onClick={handleSubmit}>
+            Отправить данные
+          </Button>
+        </section>
+      )}
+    </>
+  );
+};
+
+export default DetailsTable;
---
+++ src/components/trains-table/trains-table.tsx
@@ -0,0 +1,48 @@
+import { useEffect, FC } from "react";
+import { Table } from "react-bootstrap";
+import styles from "./app.module.css";
+import {
+  selectTrains,
+  fetchTrains,
+  selectTrain,
+} from "../../services/reducers/trainSlice";
+
+import { useAppDispatch, useAppSelector } from "../../hooks";
+
+const TrainsTable: FC = () => {
+  const dispatch = useAppDispatch();
+  const trains = useAppSelector(selectTrains);
+
+  useEffect(() => {
+    dispatch(fetchTrains());
+  }, [dispatch]);
+
+  const handleTrainClick = (id: string) => {
+    dispatch(selectTrain({ id: id }));
+  };
+  return (
+    <section className={styles.trains}>
+      <h1>Поезда</h1>
+      <Table bordered hover>
+        <thead>
+          <tr>
+            <th>Название</th>
+            <th>Описание</th>
+          </tr>
+        </thead>
+        <tbody>
+          {trains &&
+            trains.map((train) => (
+              <tr key={train.id} onClick={() => handleTrainClick(train.id)}>
+                <td>{train.name}</td>
+                <td>{train.description}</td>
+              </tr>
+            ))}
+        </tbody>
+      </Table>
+    </section>
+  );
+};
+
+export default TrainsTable;
+
